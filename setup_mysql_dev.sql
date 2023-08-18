fd772fddedf1370249e7d3c801dd62b4b1250905
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-18 23:16:17 +0100

    dyrane is awesome

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -9,9 +9,14 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant privileges to the user on the specific databases
+
 -- 'hbnb_dev' is granted all privileges on the 'hbnb_dev_db' database
--- 'hbnb_dev' is granted SELECT privilege on the 'performance_schema' database
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Refresh privileges to apply changes
+FLUSH PRIVILEGES;
+
+-- 'hbnb_dev' is granted SELECT privilege on the 'performance_schema' database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 -- Refresh privileges to apply changes
