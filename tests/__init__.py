25de0bd86a3425ca1d362728121d863980c638af
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-18 22:52:58 +0100

    dyrane is awesome

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,6 +5,7 @@ import os
 from typing import TextIO
 from models.engine.file_storage import FileStorage
 
+
 def clear_stream(stream: TextIO):
     """Clears the contents of a given stream.
 
@@ -15,6 +16,7 @@ def clear_stream(stream: TextIO):
         stream.seek(0)
         stream.truncate(0)
 
+
 def delete_file(file_path: str):
     """Deletes a file if it exists.
 
@@ -23,6 +25,7 @@ def delete_file(file_path: str):
     """
     if os.path.isfile(file_path):
         os.unlink(file_path)
+
 
 def reset_store(store: FileStorage, file_path='file.json'):
     """Resets the items in the given store.
@@ -35,6 +38,7 @@ def reset_store(store: FileStorage, file_path='file.json'):
         file.write('{}')
         if store is not None:
             store.reload()
+
 
 def read_text_file(file_name):
     """Reads the contents of a given file.
@@ -51,6 +55,7 @@ def read_text_file(file_name):
             for line in file.readlines():
                 lines.append(line)
     return ''.join(lines)
+
 
 def write_text_file(file_name, text):
     """Writes text to a given file.
