8802976af5830b9aaa41196975aaa87ad243c095
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-18 22:15:15 +0100

    dyrane is awesome

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,7 +10,7 @@ import unittest
 from io import StringIO
 from unittest.mock import patch
 
-from console import HBNBCommand
+from ..console import HBNBCommand
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
