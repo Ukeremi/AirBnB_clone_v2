26d92dcd381ec10786d6c388fe246c4d43294cc4
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-20 20:59:15 +0100

    dyrane is awesome

--- models/user.py
+++ models/user.py
@@ -12,57 +12,58 @@ from sqlalchemy import Column, String
 class User(BaseModel, Base):
     """Representation of a User"""
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
+    #if getenv('HBNB_TYPE_STORAGE') == 'db':
+    #    __tablename__ = 'users'
 
-        email = Column(
-            String(128),
-            nullable=False
-        )
+    __tablename__ = 'users'
+    email = Column(
+        String(128),
+        nullable=False
+    )
 
-        _password = Column(
-            'password',
-            String(128),
-            nullable=False
-        )
+    _password = Column(
+        'password',
+        String(128),
+        nullable=False
+    )
 
-        first_name = Column(
-            String(128),
-            nullable=True
-        )
+    first_name = Column(
+        String(128),
+        nullable=True
+    )
 
-        last_name = Column(
-            String(128),
-            nullable=True
-        )
+    last_name = Column(
+        String(128),
+        nullable=True
+    )
 
-        places = relationship(
-            "Place",
-            backref="user",
-            cascade="all,delete-orphan"
-        )
+    places = relationship(
+        "Place",
+        backref="user",
+        cascade="all,delete-orphan"
+    )
 
-        reviews = relationship(
-            "Review",
-            backref="user",
-            cascade="all, delete-orphan"
-        )
-    else:
-        email = ""
-        _password = ""
-        first_name = ""
-        last_name = ""
+    reviews = relationship(
+        "Review",
+        backref="user",
+        cascade="all, delete-orphan"
+    )
+    #else:
+    #    email = ""
+    #    _password = ""
+    #    first_name = ""
+    #    last_name = ""
 
-    def __init__(self, *args, **kwargs):
-        """Initializes User"""
-        super().__init__(*args, **kwargs)
+    #def __init__(self, *args, **kwargs):
+    #    """Initializes User"""
+    #    super().__init__(*args, **kwargs)
 
-    @property
-    def password(self):
-        """Getter for the password attribute"""
-        return self._password
+    #@property
+    #def password(self):
+    #    """Getter for the password attribute"""
+    #    return self._password
 
-    @password.setter
-    def password(self, pwd):
-        """Setter for the password attribute, hashes the password"""
-        self._password = hashlib.md5(pwd.encode()).hexdigest()
+    #@password.setter
+    #def password(self, pwd):
+    #    """Setter for the password attribute, hashes the password"""
+    #    self._password = hashlib.md5(pwd.encode()).hexdigest()
