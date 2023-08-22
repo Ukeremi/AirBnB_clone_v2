6d44d9e1356d43cbd15f85aaf3067297413ceeb4
Ukpono <ukpono9@gmail.com>
2023-08-22 13:06:39 -0700

    update user

--- models/user.py
+++ models/user.py
@@ -1,31 +1,68 @@
 #!/usr/bin/python3
+"""Module that holds the User class"""
 
-"""
-A module that defines the ORM class for User table
-"""
+import hashlib
+import models
+from models.base_model import BaseModel, Base
 from os import getenv
-from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
-    """
-    Defines attributes for User table
-    """
-    __tablename__ = 'users'
+    """Representation of a User"""
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
+        __tablename__ = 'users'
+
+        email = Column(
+            String(128),
+            nullable=False
+        )
+
+        _password = Column(
+            'password',
+            String(128),
+            nullable=False
+        )
+
+        first_name = Column(
+            String(128),
+            nullable=True
+        )
+
+        last_name = Column(
+            String(128),
+            nullable=True
+        )
+
         places = relationship(
-            'Place', backref='user', cascade='all, delete')
+            "Place",
+            backref="user",
+            cascade="all,delete-orphan"
+        )
+
         reviews = relationship(
-            'Review', backref='user', cascade='all, delete')
+            "Review",
+            backref="user",
+            cascade="all, delete-orphan"
+        )
     else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+        email = ""
+        _password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes User"""
+        super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        """Getter for the password attribute"""
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """Setter for the password attribute, hashes the password"""
+        self._password = hashlib.md5(pwd.encode()).hexdigest()
