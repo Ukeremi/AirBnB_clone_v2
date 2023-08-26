d00352deb2c1fab5c50f9850c1b31ca809af6783
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-26 12:23:55 +0100

    dyrane is awesome

--- console.py
+++ console.py
@@ -309,10 +309,11 @@ class HBNBCommand(cmd.Cmd):
 
     def do_count(self, args):
         """Count current number of class instances"""
-        count = 0
-        for k, v in storage._FileStorage__objects.items():
-            if args == k.split('.')[0]:
-                count += 1
+        if args not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        count = storage.count(args)
         print(count)
 
     def help_count(self):
