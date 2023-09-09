34bed7beb915a4f75609876c734616e3f6a6fe0b
Ukpono <ukpono9@gmail.com>
2023-09-09 09:17:26 -0700

    deploying web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,62 +3,50 @@
 Distributes an archive to my web servers,
 using the function do_deploy
 """
-
 from fabric.api import *
 from datetime import datetime
-from os import path
+import os
 
-# Define the host servers and user
 env.hosts = ['35.175.132.181', '52.91.126.56']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+
+
+def do_pack():
+    '''
+    Generates a tgz archive from the
+    contents of the web_static folder
+    '''
+    try:
+        local('mkdir -p versions')
+        datetime_format = '%Y%m%d%H%M%S'
+        archive_path = 'versions/web_static_{}.tgz'.format(
+            datetime.now().strftime(datetime_format))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        print('web_static packed: {} -> {}'.format(archive_path,
+              os.path.getsize(archive_path)))
+    except:
+        return None
 
 
 def do_deploy(archive_path):
-    """
-    Deploys the web_static archive to the web servers.
-
-    Args:
-        archive_path (str): The path to the web_static archive.
-
-    Returns:
-        bool: True if deployment is successful, False otherwise.
-    """
+    '''
+    Deploy archive to web server
+    '''
+    if not os.path.exists(archive_path):
+        return False
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
     try:
-        if not path.exists(archive_path):
-            return False
-
-        # Extract the filename from the archive path
-        filename = archive_path.split("/")[-1]
-        # Remove the file extension (.tgz) to get the timestamp
-        timestamp = filename[:-4]
-
-        # Create paths
-        archive_src = "/tmp/" + filename
-        archive_dst = "/data/web_static/releases/" + timestamp
-
-        # Upload the archive
-        put(archive_path, archive_src)
-
-        # Create the destination directory
-        run("sudo mkdir -p {}".format(archive_dst))
-
-        # Extract the archive
-        run("sudo tar -xzf {} -C {}".format(archive_src, archive_dst))
-
-        # Delete the archive
-        run("sudo rm {}".format(archive_src))
-
-        # Move contents to the correct location
-        run("sudo mv {}/web_static/* {}".format(archive_dst, archive_dst))
-
-        # Remove the old symbolic link
-        run("sudo rm -rf /data/web_static/current")
-
-        # Create a new symbolic link
-        run("sudo ln -s {} /data/web_static/current".format(archive_dst))
-
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
         return True
-
-    except Exception as e:
+    except:
         return False
