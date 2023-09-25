5931561e9c6ca0a53ccb55243860fa2b97a7f4d6
Ukpono <ukpono9@gmail.com>
2023-09-25 00:38:12 -0700

    update

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,34 +1,27 @@
-<!doctype html>
-<html lang="en">
+#!/usr/bin/python3
+""" Starts a Flask web app """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
 
-<head>
-  <title>HBNB</title>
-</head>
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-<body>
 
-  {% if id %}
-  {% if id in states %}
-  {% set state = states[id] %}
-  <h1>State: {{state.name}}</h1>
-  <h3>Cities:</h3>
-  <ul>
-    {% for city in state.cities|sort(attribute='name') %}
-    <li>{{city.id}}: <b>{{city.name}}</b></li>
-    {% endfor %}
-  </ul>
-  {% else %}
-  <h1>Not found!</h1>
-  {% endif %}
-  {% else %}
-  <h1>States</h1>
-  <ul>
-    {% for state in states.values()|list|sort(attribute='name') %}
-    <li>{{state.id}}: <b>{{state.name}}</b></li>
-    {% endfor %}
-  </ul>
-  {% endif %}
+@app.teardown_appcontext
+def dispose(exception):
+    """ Remove current session """
+    storage.close()
 
-</body>
 
-</html>
+@app.route('/states/')
+@app.route('/states/<id>')
+def states_and_state(id=None):
+    """ Display list of all the states """
+    if id:
+        id = 'State.{}'.format(id)
+    return render_template('9-states.html', states=storage.all(State), id=id)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
